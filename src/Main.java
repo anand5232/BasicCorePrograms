7266dae0952fcf70765f46d8aac731e2c496fdb2
anand5232 <jhariyaanand000@gmail.com>
2023-05-19 16:36:53 +0530

    Added: Quotient and Remainder

--- src/Main.java
+++ src/Main.java
@@ -87,7 +87,20 @@ public class Main {
     }
 
 
+    public static void quotientRemainder() {
+
+        int dividend = 25, divisor = 4;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
     }
 
 
-}
+
+
+    }
+
+
