19dc2f6df4f37969272a6230f3688e0f2cd5f6dc
anand5232 <jhariyaanand000@gmail.com>
2023-05-19 16:15:24 +0530

    Added: Leap Year

--- src/Main.java
+++ src/Main.java
@@ -41,4 +41,19 @@ public class Main {
 
     }
 
+
+    public static void leapYear(){
+        int year;
+        System.out.println("Enter an Year ");
+        Scanner sc = new Scanner(System.in);
+        year = sc.nextInt();
+
+        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+            System.out.println(" It is a leap year");
+        else
+            System.out.println("It is not a leap year");
+    }
+
+
+
 }
