a9e7bd6af5e2fad4202a5a370be0f25629270b7a
anand5232 <jhariyaanand000@gmail.com>
2023-05-19 16:50:19 +0530

    Added: Largest Among Three Numbers

--- src/Main.java
+++ src/Main.java
@@ -146,6 +146,21 @@ public class Main {
 
 
 
+    public static void LargestNumber() {
+
+        double n1 = -4.5, n2 = 3.9, n3 = 2.5;
+
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println("Largest Number = "+n1);
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println("Largest Number = "+n2);
+
+        else
+            System.out.println("Largest Number = "+n3);
+    }
+
+
     }
 
 
