ae14f0db3ba0dc53c4b2d1c8b065a9bfffb9cf25
anand5232 <jhariyaanand000@gmail.com>
2023-05-19 16:47:25 +0530

    Added: Alphabet is Vowel or Consonant

--- src/Main.java
+++ src/Main.java
@@ -131,6 +131,20 @@ public class Main {
     }
 
 
+    public static void vowelConsonant() {
+
+        System.out.println("Enter Any Character");
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(1);
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+
+
 
     }
 
